7e54ce2156354f0464cf66f6351e864acf60e58f
AthenaCaesura <athena.caesura@zapatacomputing.com>
2022-01-13 21:00:11 -0500

    fix: warnings if QVM and QUILC  already started

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,16 @@
+import pytest
+
 from qeforest.simulator import kill_subprocesses
 
 
 def pytest_sessionfinish(session, exitstatus):
     kill_subprocesses()
+
+
+def pytest_runtestloop(session):
+    session.add_marker(
+        pytest.mark.filterwarnings("ignore:Port 5000 which is used to run QVM")
+    )
+    session.add_marker(
+        pytest.mark.filterwarnings("ignore:Port 5555 which is used to run QUILC")
+    )
