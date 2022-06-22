d3ca85ebfdd8860ec494aeff387005cf56b1cd62
James Clark <james.clark@zapatacomputing.com>
2022-06-22 14:40:50 +0100

    fix: fix incompatible setup_requires in setup.py with build-system.requires in pyproject.toml

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    setup_requires=["setuptools_scm~=6.0"],
     install_requires=[
         "z-quantum-core",
         "pyquil~=2.25",
